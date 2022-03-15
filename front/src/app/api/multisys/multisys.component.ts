853fe4f9e080d3645ce0f39ccdc1db00fe905633
alejandro <85931059+adamecate@users.noreply.github.com>
2022-03-15 19:36:05 +0800

    new update

--- front/src/app/api/multisys/multisys.component.ts
+++ front/src/app/api/multisys/multisys.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-multisys',
   templateUrl: './multisys.component.html',
-  styleUrls: ['./multisys.component.css']
+  styleUrls: ['./multisys.component.scss']
 })
 export class MultisysComponent implements OnInit {
 
