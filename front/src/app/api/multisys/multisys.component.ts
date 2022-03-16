60661d31995d68bf2103704145dde4a1341b84e1
adame cate <85931059+adamecate@users.noreply.github.com>
2022-03-15 19:19:04 -0700

    multisys added
    ---

---
+++ front/src/app/api/multisys/multisys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multisys',
+  templateUrl: './multisys.component.html',
+  styleUrls: ['./multisys.component.css']
+})
+export class MultisysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
